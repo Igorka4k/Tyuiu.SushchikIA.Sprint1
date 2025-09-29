2cdfec0dd278f5c39822c89accd7d370e46e05fc
Igorase <garik.sushik@ya.ru>
2025-09-30 00:48:39 +0500

    Task 1

---
+++ Tyuiu.SushchikIA.Sprint1.Task1.V8.Test/Test1.cs
@@ -0,0 +1,14 @@
+﻿using Tyuiu.SushchikIA.Sprint1.Task1.V8.Lib;
+namespace Tyuiu.SushchikIA.Sprint1.Task1.V8.Test
+{
+    [TestClass]
+    public sealed class Test1
+    {
+        [TestMethod]
+        public void ValueExpression()
+        {
+            DataService ds = new DataService();
+            Assert.AreEqual((3 * Math.PI) / 1, ds.Calculate(1, 3));
+        }
+    }
+}
