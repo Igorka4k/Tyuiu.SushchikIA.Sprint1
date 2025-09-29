7ddb3d3322726c88385ba2a02c8489d9e7c4541d
Igorase <garik.sushik@ya.ru>
2025-09-29 21:43:33 +0500

    Добавьте файлы проекта.

---
+++ Tyuiu.SushchikIA.Sprint1.Task0.V14.Test/Test1.cs
@@ -0,0 +1,15 @@
+﻿using Tyuiu.SushchikIA.Sprint1.Task0.V14.Lib;
+namespace Tyuiu.SushchikIA.Sprint1.Task0.V14.Test
+{
+    [TestClass]
+    public sealed class Test1
+    {
+        [TestMethod]
+        public void ValidExpression()
+        {
+            DataService ds = new DataService();
+            var res = ds.Calculate();
+            Assert.AreEqual(25, res);
+        }
+    }
+}
