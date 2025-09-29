085dec376747cb472485c84a73163e916e8ee68a
Igorase <garik.sushik@ya.ru>
2025-09-30 02:46:37 +0500

    Task 6

---
+++ Tyuiu.SushchikIA.Sprint1.Task6.V16.Lib/Class1.cs
@@ -0,0 +1,13 @@
+﻿using tyuiu.cources.programming.interfaces.Sprint1;
+using static System.Net.Mime.MediaTypeNames;
+namespace Tyuiu.SushchikIA.Sprint1.Task6.V16.Lib
+{
+    public class DataService : ISprint1Task6V16
+    {
+        public bool CheckSpecSymbols(string value)
+        {
+            bool flag = value.Contains('!') && value.Contains('?');
+            return flag;
+        }
+    }
+}
---
+++ Tyuiu.SushchikIA.Sprint1.Task6.V16.Test/Test1.cs
@@ -0,0 +1,15 @@
+﻿using Tyuiu.SushchikIA.Sprint1.Task6.V16.Lib;
+
+namespace Tyuiu.SushchikIA.Sprint1.Task6.V16.Test
+{
+    [TestClass]
+    public sealed class Test1
+    {
+        [TestMethod]
+        public void ValueExpression()
+        {
+            DataService ds = new DataService();
+            Assert.AreEqual(true, ds.CheckSpecSymbols("!!!eifje*??f?ifeoj!!!!"));
+        }
+    }
+}
