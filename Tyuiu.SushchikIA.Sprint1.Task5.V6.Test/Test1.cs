a5c38800b6d24fea85e58c2419ec47484f705a24
Igorase <garik.sushik@ya.ru>
2025-09-30 02:35:38 +0500

    Task 5

---
+++ Tyuiu.SushchikIA.Sprint1.Task5.V6.Test/Test1.cs
@@ -0,0 +1,14 @@
+﻿using Tyuiu.SushchikIA.Sprint1.Task5.V6.Lib;
+namespace Tyuiu.SushchikIA.Sprint1.Task5.V6.Test
+{
+    [TestClass]
+    public sealed class Test1
+    {
+        [TestMethod]
+        public void ValueExpression()
+        {
+            DataService ds = new DataService();
+            Assert.AreEqual(6, ds.Calculate(13));
+        }
+    }
+}
---
+++ Tyuiu.SushchikIA.Sprint1.Task5.V6/Program.cs
@@ -0,0 +1,27 @@
+﻿using Tyuiu.SushchikIA.Sprint1.Task5.V6.Lib;
+
+
+DataService ds = new DataService();
+Console.Title = "Спринт #1 | Выполнил: Сущик И.А. | ПИНб-25-1";
+Console.WriteLine("***************************************************************************");
+Console.WriteLine("* Спринт #1                                                               *");
+Console.WriteLine("* Тема: Базовые навыки работы в C#                                        *");
+Console.WriteLine("* Задание #5                                                              *");
+Console.WriteLine("* Вариант #6                                                             *");
+Console.WriteLine("* Выполнил: Сущик Игорь Александрович | ПИНб-25-1                         *");
+Console.WriteLine("***************************************************************************");
+Console.WriteLine("* УСЛОВИЕ:                                                                *");
+Console.WriteLine("* Написать программу, которая запрашивает у пользователя исходные данные, *");
+Console.WriteLine("* выполняет указанные расчёты и печатает результат на экране.             *");
+Console.WriteLine("*                                                                         *");
+Console.WriteLine("***************************************************************************");
+Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+string value = Console.ReadLine();
+int intValue = Convert.ToInt32(value);
+Console.WriteLine("***************************************************************************");
+Console.WriteLine("*                                                                         *");
+Console.WriteLine("***************************************************************************");
+Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+Console.WriteLine("***************************************************************************");
+Console.WriteLine(ds.Calculate(intValue));
+Console.ReadLine();
